e00a7b6e21e17e87558f1b323d1b773d8f31e86a
outs1der <jcsanchez.martinez.2020@gmail.com>
2023-03-14 19:11:20 +0000

    3/14/23

--- main.js
+++ main.js
@@ -3,20 +3,42 @@ const menuHamburguer = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('product-detail');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguer.addEventListener('click', toggleMobileMenu);
-menuCartIcon.addEventListener('click', toggleCartMenu())
+menuCartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu(){
+	const asideClose = aside.classList.contains('inactive');
+	
+	if (! asideClose){
+		aside.classlist.add('inactive');
+	}
+
 	desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+	const asideClose = aside.classList.contains('inactive');
+	
+	if (! asideClose){
+		aside.classlist.add('inactive');
+	}
+
 	mobileMenu.classList.toggle('inactive');
+
 }
 
 function toggleCartMenu(){
+
+	const mobileMenuClose = mobileMenu.classList.contains('inactive');
+
+	if (! mobileMenuClose){
+		mobileMenu.classList.add('inactive');
+	}
+
 	aside.classList.toggle('inactive');
+	
+	
 }
